2ecf06b4357bf5d95fd2df0e71ff8c4e4aebd6d7
Radek Cieciwa <radoslaw.cieciwa@team.bumble.com>
2025-08-25 11:09:48 +0200

    Added TCA initial project.

---
+++ Project.swift
@@ -0,0 +1,36 @@
+import ProjectDescription
+
+let project = Project(
+    name: "TCA-Modularisation-Tuist",
+    targets: [
+        .target(
+            name: "TCA-Modularisation-Tuist",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.TCA-Modularisation-Tuist",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["TCA-Modularisation-Tuist/Sources/**"],
+            resources: ["TCA-Modularisation-Tuist/Resources/**"],
+            dependencies: [
+                .external(name: "ComposableArchitecture"),
+            ]
+        ),
+        .target(
+            name: "TCA-Modularisation-TuistTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.TCA-Modularisation-TuistTests",
+            infoPlist: .default,
+            sources: ["TCA-Modularisation-Tuist/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "TCA-Modularisation-Tuist")]
+        ),
+    ]
+)
---
+++ TCA-Modularisation-Tuist/Sources/MainWindow.swift
@@ -0,0 +1,8 @@
+//
+//  MainWindow.swift
+//  TCA-Modularisation-Tuist
+//
+//  Created by Radek Cieciwa on 24/08/2025.
+//
+
+import SwiftUI

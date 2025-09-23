f39241cbb912c5a6114aee7d3a8fae22fd24cfba
Radek Cieciwa <radoslaw.cieciwa@team.bumble.com>
2025-09-23 16:08:55 +0200

    Fixed generation.

--- Tuist/ProjectDescriptionHelpers/GlobalSettings.swift
+++ Tuist/ProjectDescriptionHelpers/GlobalSettings.swift
@@ -2,4 +2,6 @@ import ProjectDescription
 
 enum GlobalSettings {
     static let iOSMinimumDeploymentTarget = 18.0
+    static let organizationName: String = "tuist"
+    static let organisationIdentifier: String = "io.tuist"
 }
